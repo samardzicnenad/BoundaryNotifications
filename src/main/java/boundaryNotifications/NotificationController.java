5254fd4698e4e46ec6ac495ebfed1d940bc0464b
Nenad Samardzic <samardzicnenad@gmail.com>
2015-01-31 14:46:45 -0500

    commit_03: Added GET endpoint

--- src/main/java/boundaryNotifications/NotificationController.java
+++ src/main/java/boundaryNotifications/NotificationController.java
@@ -5,15 +5,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 @RestController
 public class NotificationController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    // /notifications/by_user/<user_id>(?limit=<limit_value>)
+	@RequestMapping(value="/notifications/by_user/{user_id}", method=RequestMethod.GET)
+	public Notification[] getRecent(@PathVariable int user_id, @RequestParam(value="limit", defaultValue="3") int limitNo) {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass(org.h2.Driver.class);
+		dataSource.setUrl("jdbc:h2:boundary");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
 
-    @RequestMapping("/test")
-    public Notification test(@RequestParam(value="name", defaultValue="Hackeratus") String name) {
-		int ts = (int) (System.currentTimeMillis() / 1000L);
-        return new Notification(counter.incrementAndGet(), 1, ts, String.format(template, name));
-    }
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        List<Notification> resultList = jdbcTemplate.query(
+                "select * from notifications where user_id = ? order by timestamp desc limit ?",
+                new Object[] {user_id, limitNo},
+                new RowMapper<Notification>() {
+                    @Override
+                    public Notification mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return new Notification(rs.getLong("id"), rs.getInt("user_id"), rs.getInt("timestamp"), rs.getString("message"));
+                    }
+                });
+		Notification[] notifications = new Notification[resultList.size()];
+		notifications = resultList.toArray(notifications);
+        return notifications;
+	}
 }
